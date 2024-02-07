77617c626c5f632c8b221f918ff8df0c41824c87
nickllama <llama.gradual-0j@icloud.com>
2024-02-07 14:24:37 +0100

    ДЗ 21.1.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 
 from catalog.apps import CatalogConfig
-from catalog.views import categories, category_products, product_detail, index
+from catalog.views import CategoryListView, ProductListView, product_detail, index
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
-    path('categories/', categories, name='categories'),
-    path('category/<int:category_id>/', category_products, name='category_products'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('category/<int:pk>/', ProductListView.as_view(), name='category_products'),
     path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .forms import FeedbackForm
 from .models import Category, Product
 
@@ -11,22 +13,45 @@ def index(request):
     return render(request, 'catalog/index.html', context)
 
 
-def categories(request):
-    context = {
-        'object_list': Category.objects.all(),
-        'title': 'Категории товаров'
-    }
-    return render(request, 'catalog/categories.html', context)
+# def categories(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'Категории товаров'
+#     }
+#     return render(request, 'catalog/category_list.html', context)
 
 
-def category_products(request, category_id):
-    category_item = Category.objects.get(pk=category_id)
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {'title': 'Категории товаров'}
 
-    context = {
-        'object_list': Product.objects.filter(product_category=category_item),
-        'title': f'Все товары категории: {category_item.category_name}'
-    }
-    return render(request, 'catalog/Products.html', context)
+
+# def category_products(request, pk):
+#     category_item = Category.objects.get(pk=pk)
+#
+#     context = {
+#         'object_list': Product.objects.filter(product_category_id=pk),
+#         'title': f'Все товары категории: {category_item.category_name}'
+#     }
+#     return render(request, 'catalog/product_list.html', context)
+
+
+class ProductListView(ListView):
+    model = Product
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(product_category_id=self.kwargs('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data['category_pk']: category_item.pk
+        context_data['title'] = f'Все товары категории: {category_item.category_name}'
+
+        return context_data
 
 
 def product_detail(request, product_id):
