bd8a27517b450bcf3ff8ed943bd6b4b505a0bbf7
nickllama <llama.gradual-0j@icloud.com>
2024-02-07 17:52:35 +0100

    21.1 FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from catalog.apps import CatalogConfig
-from catalog.views import CategoryListView, ProductListView, product_detail, index
+from catalog.views import CategoryListView, ProductListView, ProductDetailView, index
 
 app_name = CatalogConfig.name
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('category/<int:pk>/', ProductListView.as_view(), name='category_products'),
-    path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
-
-from .forms import FeedbackForm
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Category, Product
 
 
@@ -13,12 +11,8 @@ def index(request):
     return render(request, 'catalog/index.html', context)
 
 
-# def categories(request):
-#     context = {
-#         'object_list': Category.objects.all(),
-#         'title': 'Категории товаров'
-#     }
-#     return render(request, 'catalog/category_list.html', context)
+class ProductDetailView(DetailView):
+    model = Product
 
 
 class CategoryListView(ListView):
@@ -26,22 +20,12 @@ class CategoryListView(ListView):
     extra_context = {'title': 'Категории товаров'}
 
 
-# def category_products(request, pk):
-#     category_item = Category.objects.get(pk=pk)
-#
-#     context = {
-#         'object_list': Product.objects.filter(product_category_id=pk),
-#         'title': f'Все товары категории: {category_item.category_name}'
-#     }
-#     return render(request, 'catalog/product_list.html', context)
-
-
 class ProductListView(ListView):
     model = Product
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset.filter(product_category_id=self.kwargs('pk'))
+        queryset = queryset.filter(product_category_id=self.kwargs.get('pk'))
         return queryset
 
     def get_context_data(self, *args, **kwargs):
@@ -52,27 +36,3 @@ class ProductListView(ListView):
         context_data['title'] = f'Все товары категории: {category_item.category_name}'
 
         return context_data
-
-
-def product_detail(request, product_id):
-    product = Product.objects.get(pk=product_id)
-    return render(request, 'catalog/product_detail.html', {'product': product})
-
-
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
-
-
-def FeedbackForm(request):
-    if request.method == 'POST':
-        form = FeedbackForm(request.POST)
-        if form.is_valid():
-            # Обработка данных формы (например, сохранение в базу данных)
-            # Здесь можно добавить логику для обработки отправленной обратной связи
-
-            # После обработки формы, можно, например, перенаправить пользователя
-            return render(request, 'thank_you_page.html')
-    else:
-        form = FeedbackForm()
-
-    return render(request, 'contacts.html', {'form': form})
