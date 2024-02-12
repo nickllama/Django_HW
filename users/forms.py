c646b5f8ec7654d0bec232bcef6c1ba1f79ec4f1
nickllama <llama.gradual-0j@icloud.com>
2024-02-12 16:38:11 +0100

    22.2.2 update style forms

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.forms import HiddenInput
 
+from catalog.forms import StyleFormMixin
 from users.models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'password1', 'password2']
 
 
-class UserUpdateForm(UserChangeForm):
+class UserUpdateForm(StyleFormMixin, UserChangeForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'user_phone', 'user_avatar', 'user_country')
